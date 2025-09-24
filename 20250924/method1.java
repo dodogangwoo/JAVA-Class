62424cb9f1ae728f011c64885ae6358606b89ee0
dogangwoo@naver.com <dogangwoo@naver.com>
2025-09-24 11:55:52 +0900

    add method

---
+++ 20250924/method1.java
@@ -0,0 +1,34 @@
+public class method1 {
+
+    public static void main(String[] args) {
+     
+
+        // 간단한 계산
+        int num3 = 10;
+        int num4 = 5;
+        int sum = num3 + num4;
+
+        System.out.println("\n=== 간단한 계산 ===");
+        System.out.println(num3 + " + " + num4 + " = " + sum);
+
+        // 함수(메서드) 호출
+        printCalculation(num3, num4, sum);
+        sum_printresult(num3, num4);
+        printCalculation2(num3, num4, sum);
+    }
+
+    public static void printCalculation(int number1, int number2, int result) {
+        System.out.println("\n=== 간단한 계산 ===");
+        System.out.println(number1 + " + " + number2 + " = " + result);
+    }
+    public static void sum_printresult(int number1, int number2){
+        int result = number1 + number2;
+        System.out.println("\n=== 더하기 계산 ===");
+        System.out.println(number1 + " + " + number2 + " = " + result);
+    }
+    // 계산 결과를 출력하는 private 메서드
+    private static void printCalculation2(int number1, int number2, int result) {
+        System.out.println("\n=== 간단한 계산 ===");
+        System.out.println(number1 + " + " + number2 + " = " + result);
+    }
+}
